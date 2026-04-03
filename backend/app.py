d5e99f6eca9fc5aceb96c3735421543dc8d63b45
yasuoiwakura <8234100+yasuoiwakura@users.noreply.github.com>
2026-04-03 20:35:05 +0200

    First working CORS example - browser does not show cors headers (todo debug) but since NO CORS WARNINGS show up except when calling an unmapped /urlpath this looks good for comparing.
    Squashed commit of the following:
    
    commit 108e453b7eba9ca98d252fd224aba01a2a191a20
    Author: yasuoiwakura <8234100+yasuoiwakura@users.noreply.github.com>
    Date:   Fri Apr 3 20:32:25 2026 +0200
    
        backend uses /api to test /nonexistent
    
    commit a57605afa9f89301559e7a9ed750f285e9d932f6
    Author: yasuoiwakura <8234100+yasuoiwakura@users.noreply.github.com>
    Date:   Fri Apr 3 20:30:50 2026 +0200
    
        better tests
    
    commit 5cb25e2ceb6b07729122dba9a6abf51433ab2247
    Author: yasuoiwakura <8234100+yasuoiwakura@users.noreply.github.com>
    Date:   Fri Apr 3 20:14:41 2026 +0200
    
        green notice file://
    
    commit a4f2485902899a1cdfa942a2b769c7c245403631
    Author: yasuoiwakura <8234100+yasuoiwakura@users.noreply.github.com>
    Date:   Fri Apr 3 20:12:27 2026 +0200
    
        frontend test
    
    commit d0aae2fea19e5b44e2d783cb2c6af0065922bc9f
    Author: yasuoiwakura <8234100+yasuoiwakura@users.noreply.github.com>
    Date:   Fri Apr 3 20:10:48 2026 +0200
    
        lambda test cases
    
    commit b185fa7a70fee13cad9c109b31b8e156e629ded6
    Author: yasuoiwakura <8234100+yasuoiwakura@users.noreply.github.com>
    Date:   Fri Apr 3 19:59:14 2026 +0200
    
        run.bat frontend python webserver
    
    commit 8f743e021eb75af5a6bfec54b52d3df67eef0c8a
    Author: yasuoiwakura <8234100+yasuoiwakura@users.noreply.github.com>
    Date:   Fri Apr 3 19:47:35 2026 +0200
    
        testfile htm
    
    commit e3204f093d05252f06efee9c260e1985476b1746
    Author: yasuoiwakura <8234100+yasuoiwakura@users.noreply.github.com>
    Date:   Fri Apr 3 19:46:26 2026 +0200
    
        insource config.js
    
    commit d6d5f3a2118489e8f988e7b067f807d183895875
    Author: yasuoiwakura <8234100+yasuoiwakura@users.noreply.github.com>
    Date:   Fri Apr 3 19:38:08 2026 +0200
    
        SAM: switch from aws proxy to HTTP API

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,17 @@
+import time
+
+
 def lambda_handler(event, context):
-    return {
-        "statusCode": 200,
-        "body": "Hello from Python 3.14 Lambda!"
-    }
+    params = event.get("queryStringParameters", {}) or {}
+
+    if params.get("except") == "true":
+        raise Exception("Intentional exception from Lambda")
+
+    if params.get("status") == "500":
+        return {"statusCode": 500, "body": "Intentional 500 from Lambda"}
+
+    if params.get("timeout") == "true":
+        time.sleep(35)
+        return {"statusCode": 200, "body": "Should never reach here"}
+
+    return {"statusCode": 200, "body": "Hello from Python 3.14 Lambda!"}
