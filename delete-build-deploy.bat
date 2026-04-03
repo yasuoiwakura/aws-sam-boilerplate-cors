a77791289f6e8618701a1de92e26391f3ff386d7
yasuoiwakura <8234100+yasuoiwakura@users.noreply.github.com>
2026-04-03 15:48:36 +0200

    call samb.at for PS exit code

--- delete-build-deploy.bat
+++ delete-build-deploy.bat
@@ -1,5 +1,5 @@
 rmdir /s /q ".aws-sam"
 
-sam build --no-use-container
+call sam build --no-use-container
 
-sam deploy --profile=myawsprofile --force-upload --no-confirm-changeset
+call sam deploy --profile=myawsprofile --force-upload --no-confirm-changeset
